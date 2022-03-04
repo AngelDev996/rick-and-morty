304ebd6180b42c2087448b6bb3c7ece106f7858d
AngelDev996 <arivalladares2.0@gmail.com>
2022-03-04 04:20:25 -0600

    update

---
+++ src/components/ListCharacters.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <div class="characters">
+            <div class="characters__item">
+                <h2>Characters</h2>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import{ useStore} from 'vuex'
+import store from '../store'
+export default {
+    setup(){
+        const store = useStore()
+    }
+}
+</script>
+
+<style>
+</style>
